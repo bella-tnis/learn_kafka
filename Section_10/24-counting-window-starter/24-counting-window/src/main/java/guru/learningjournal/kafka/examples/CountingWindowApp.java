e7ae49f6454de343f1f1dba52bec1568340b0a34
bella-tnis <bella.li@tnis.com>
2020-09-19 20:25:23 +0700

    start section 10

---
+++ Section_10/24-counting-window-starter/24-counting-window/src/main/java/guru/learningjournal/kafka/examples/CountingWindowApp.java
@@ -0,0 +1,40 @@
+package guru.learningjournal.kafka.examples;
+
+import guru.learningjournal.kafka.examples.serde.AppSerdes;
+import guru.learningjournal.kafka.examples.types.SimpleInvoice;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Properties;
+
+
+public class CountingWindowApp {
+    private static final Logger logger = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, AppConfigs.applicationID);
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, AppConfigs.bootstrapServers);
+        props.put(StreamsConfig.STATE_DIR_CONFIG, AppConfigs.stateStoreName);
+        props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 0);
+
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+        KStream<String, SimpleInvoice> KS0 = streamsBuilder.stream(AppConfigs.posTopicName,
+            Consumed.with(AppSerdes.String(), AppSerdes.SimpleInvoice())
+        );
+
+
+        KafkaStreams streams = new KafkaStreams(streamsBuilder.build(), props);
+        streams.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Stopping Streams");
+            streams.close();
+        }));
+
+    }
+}
