ae10f14f0121919ce54ee6a34ec1d45abfcd4c7d
bella-tnis <bella.li@tnis.com>
2020-11-21 12:54:21 +0700

    [section 8] update some scripts and code for 18-ktable

--- Section_8/18-streaming-table-starter/18-streaming-table/src/main/java/guru/learningjournal/kafka/examples/StreamingTableApp.java
+++ Section_8/18-streaming-table-starter/18-streaming-table/src/main/java/guru/learningjournal/kafka/examples/StreamingTableApp.java
@@ -18,7 +18,7 @@ public class StreamingTableApp {
     private static final Logger Logger = LogManager.getLogger();
 
     public static void main(final String[] args) {
-
+        // make a new prop
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, AppConfigs.applicationID);
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, AppConfigs.bootstrapServers);
@@ -26,23 +26,31 @@ public class StreamingTableApp {
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
+        // make a stream builder
         StreamsBuilder streamsBuilder = new StreamsBuilder();
+        // make KTable from stream builder with topic: stocl-tick
         KTable<String, String> KT0 = streamsBuilder.table(AppConfigs.topicName);
         KT0.toStream().print(Printed.<String, String>toSysOut().withLabel("KT0"));
 
-        KTable<String, String> KT1 = KT0.filter((k, v) -> k.matches(AppConfigs.regExSymbol) && !v.isEmpty(),
+        // filter out the data, we just want the key with value of HDFCBANK or TCS
+        KTable<String, String> KT1 = KT0.filter(
+                // key value lamda expression
+                (k, v) -> k.matches(AppConfigs.regExSymbol) && !v.isEmpty(),
+                // name of the state store
                 Materialized.as(AppConfigs.stateStoreName));
         KT1.toStream().print(Printed.<String, String>toSysOut().withLabel("KT1"));
 
+        // build the stream
         KafkaStreams streams = new KafkaStreams(streamsBuilder.build(), props);
 
-        //Query Server
+        //Query Server serves http://localhost:7010/kt01-store/all
         QueryServer queryServer = new QueryServer(streams, AppConfigs.queryServerHost, AppConfigs.queryServerPort);
         streams.setStateListener((newState, oldState)->{
             Logger.info("State changing to " + newState + " from " + oldState);
             queryServer.setActive(newState == KafkaStreams.State.RUNNING && oldState == KafkaStreams.State.REBALANCING);
         });
 
+        // start the sream and query server
         streams.start();
         queryServer.start();
 
